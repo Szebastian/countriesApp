a18488941b96b317ed69bb1325b201e532aae480
Szebastian <sebastianmarcoscontreras@gmail.com>
2023-12-18 21:39:39 -0300

    inicio countryApp

---
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+import { switchMap } from 'rxjs';
+
+@Component({
+  selector: 'app-country-page',
+  templateUrl: './country-page.component.html',
+  styles: ``
+})
+export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router,
+  ) { }
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.countriesService.searchCountryByAlphacode(id)),
+      )
+      .subscribe(country => {
+        if (!country) return this.router.navigateByUrl('');
+
+        return this.country = country;
+
+      });
+  }
+
+
+
+}
+
